be2c011d09aa351865d9a78f52b912d82af6471d
kushagra_9 <nagorikushagra9@gmail.com>
2020-07-17 11:20:48 +0530

    revert app changes

--- trs_filer/app.py
+++ trs_filer/app.py
@@ -1,8 +1,6 @@
 from foca.foca import foca
-from pathlib import Path
-
 
 if __name__ == '__main__':
-    config = str(Path(__file__).resolve().parent / "app_config.yaml")
     app = foca("app_config.yaml")
-    app.run(port=8080)
+    app.run(port=app.port)
+
