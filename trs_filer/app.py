0de881c0705082853ab94128a1e85c23377cfc43
kushagra_9 <nagorikushagra9@gmail.com>
2020-07-17 09:40:17 +0530

    minor fixes

--- trs_filer/app.py
+++ trs_filer/app.py
@@ -1,5 +1,8 @@
 from foca.foca import foca
+from pathlib import Path
+
 
 if __name__ == '__main__':
+    config = str(Path(__file__).resolve().parent / "app_config.yaml")
     app = foca("app_config.yaml")
-    app.run(port=app.port)
+    app.run(port=8080)
