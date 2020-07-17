2c236def36a9078c0570e00d7f5082433ed61b79
kushagra_9 <nagorikushagra9@gmail.com>
2020-07-17 09:21:26 +0530

    init endpoints; resolve spec issues

--- trs_filer/app.py
+++ trs_filer/app.py
@@ -2,4 +2,4 @@ from foca.foca import foca
 
 if __name__ == '__main__':
     app = foca("app_config.yaml")
-    app.run(port=8080)
+    app.run(port=app.port)
---
+++ trs_filer/controllers/__init__.py
@@ -0,0 +1,38 @@
+def toolsIdGet():
+    return None
+
+
+def toolsIdVersionsGet():
+    return None
+
+
+def toolsIdVersionsVersionIdGet():
+    return None
+
+
+def toolsGet():
+    return None
+
+
+def toolsIdVersionsVersionIdTypeDescriptorGet():
+    return None
+
+
+def toolsIdVersionsVersionIdTypeDescriptorRelativePathGet():
+    return None
+
+
+def toolsIdVersionsVersionIdTypeTestsGet():
+    return None
+
+
+def toolsIdVersionsVersionIdTypeFilesGet():
+    return None
+
+
+def toolsIdVersionsVersionIdContainerfileGet():
+    return None
+
+
+def toolClassesGet():
+    return None
