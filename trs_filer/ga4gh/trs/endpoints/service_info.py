b8b185dc14d182aa03680bc09c57e7955599fc4b
Kushagra <nagorikushagra9@gmail.com>
2020-09-06 02:13:28 +0530

    fix: docstrings

--- trs_filer/ga4gh/trs/endpoints/service_info.py
+++ trs_filer/ga4gh/trs/endpoints/service_info.py
@@ -10,13 +10,17 @@ logger = logging.getLogger(__name__)
 class RegisterService:
     """Tool class for registering service_info. Returns readily formatted service
     info; creates service_info database document if it does not exist.
-
-    Attributes:
-        db_collection: Database collection storing service info objects.
-        service_info: Service info details as per enpoints config.
-        collection_service_info: Latest service_info detail stored in database.
     """
+
     def __init__(self) -> None:
+        """Initialize class requirements.
+
+        Attributes:
+            db_collection: Database collection storing service info objects.
+            service_info: Service info details as per enpoints config.
+            collection_service_info: Latest service_info detail stored in
+            database.
+        """
         self.db_collection = (
             current_app.config['FOCA'].db.dbs['trsStore']
             .collections['service_info'].client
@@ -26,6 +30,12 @@ class RegisterService:
         self.collection_service_info = self.get_latest_service_info()
 
     def get_latest_service_info(self) -> Union[Dict, None]:
+        """Get latest service info details from database.
+
+        Returns:
+            Dict object corresponding the latest service info details.
+            If not found, None.
+        """
         try:
             return self.db_collection.find(
                 {},
@@ -35,6 +45,12 @@ class RegisterService:
             return None
 
     def get_service_info(self) -> Dict:
+        """Get service info details from database. If requested config,
+        not present, then creates a mapping in the database.
+
+        Returns:
+            Latest service info details from database.
+        """
         # Write current service info to db if absent/different from latest
         if not self.service_info == self.collection_service_info:
             try:
